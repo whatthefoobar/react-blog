3ec5b26d573cf2d0e12b725b22f742f8ac1f31b4
whatthefoobar <irina.mntn@gmail.com>
2022-05-04 11:01:06 +0200

    made updates

--- src/App.js
+++ src/App.js
@@ -1,56 +1,31 @@
-import Home from "../src/pages/Home";
-import TopBar from "./components/TopBar";
-import Single from "../src/pages/Single";
-import Write from "../src/pages/Write";
-import Settings from "../src/pages/Settings";
-import Login from "../src/pages/Login";
-import Register from "../src/pages/Register";
+import Home from '../src/pages/Home';
+import TopBar from './components/TopBar';
+import Single from '../src/pages/Single';
+import Write from '../src/pages/Write';
+import Settings from '../src/pages/Settings';
+import Login from '../src/pages/Login';
+import Register from '../src/pages/Register';
 
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import About from "./pages/About";
-
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
   const user = true; /*for testing purposes*/
   return (
     <Router>
-      <TopBar/>
-      <Switch>
-        <Route exact path="/">
-            <Home />
-        </Route>
-        <Route path="/about">
-          <About/>  
-        </Route>
-        <Route path="/register">
-           { user? <Home/> : <Register/>} 
-           {/* if there's a user go to home else to register */}
-        </Route>
-        <Route path="/login">
-          { user? <Home/> : <Login/>} 
-        </Route>
-        <Route path="/write">
-          { user? <Write/> : <Register/>} 
-        </Route>
-        <Route path="/settings">
-          { user? <Settings/> : <Register/>} 
-        </Route>
-        <Route path="/post/:postId">
-            <Single/>
-        </Route>
-      </Switch>
-
-
+      <TopBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/register" element={user ? <Home /> : <Register />} />{' '}
+        {/* if there's a user go to home else to register */}
+        <Route path="/login" element={user ? <Home /> : <Login />} />
+        <Route path="/write" element={user ? <Write /> : <Register />} />
+        <Route path="/settings" element={user ? <Settings /> : <Register />} />
+        <Route path="/post/:postId" element={<Single />} />
+      </Routes>
     </Router>
   );
 }
 
-
 export default App;
-
-
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,21 +1,19 @@
-import "../css/post.css"
-
+import '../css/post.css';
 
 export default function Post(props) {
-
-    return (
-        <div className="post">
-            <img className="postImg" src={props.image}  alt="autumn image" />
-            <div className="postInfo">
-                <div className="postCats">
-                    <span className="postCat">Art</span>
-                    <span className="postCat">Sport</span>
-                </div>
-                <span className="postTitle">{props.title}</span>
-                <hr />
-                <span className="postDate">1 hour ago</span>
-            </div>
-            <p className="postDescription">{props.description} </p>
+  return (
+    <div className="post">
+      <img className="postImg" src={props.image} alt="autumn" />
+      <div className="postInfo">
+        <div className="postCats">
+          <span className="postCat">Art</span>
+          <span className="postCat">Sport</span>
         </div>
-    )
+        <span className="postTitle">{props.title}</span>
+        <hr />
+        <span className="postDate">1 hour ago</span>
+      </div>
+      <p className="postDescription">{props.description} </p>
+    </div>
+  );
 }
