413778aad7303723a002e7bd21d297de897ff060
whatthefoobar <irina.mntn@gmail.com>
2022-07-31 10:18:41 +0200

    backend stuff setup

--- api/index.js
+++ api/index.js
@@ -1,26 +1,46 @@
-import express from 'express';
-import path from 'path';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require('./routes/auth');
+const userRoute = require('./routes/users');
+const postRoute = require('./routes/posts');
+const categoryRoute = require('./routes/categories');
+const multer = require('multer');
+const path = require('path');
 
 dotenv.config();
 
-// mongoose
-//   .connect(process.env.MONGODB_URI)
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//   })
-//   .catch((err) => {
-//     console.log(err.message);
-//   });
-console.log('testing testing 123');
-const app = express();
-
 app.use(express.json());
+app.use('/images', express.static(path.join(__dirname, '/images')));
 
-app.use('/foof', (req, res) => {
-  console.log('this is foof url');
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
 });
+
+const upload = multer({ storage: storage });
+app.post('/api/upload', upload.single('file'), (req, res) => {
+  res.status(200).json('File has been uploaded');
+});
+
+app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
+app.use('/api/categories', categoryRoute);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
