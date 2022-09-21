a72d09520c8fb379d58f23d87ab91c7964ebb529
whatthefoobar <irina.mntn@gmail.com>
2022-09-21 10:04:13 +0200

    prep for deploy p 2

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -5,8 +5,7 @@ const Posts = ({ posts }) => {
   return (
     <div className="posts">
       {posts.map((post) => (
-        // console.log(post._id)
-        <Post key={post._id} post={post} />
+        <Post post={post} key={post._id} />
       ))}
     </div>
   );
