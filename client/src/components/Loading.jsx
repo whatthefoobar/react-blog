bf5ba41f736d5dc0af84a40c74ee9d5524aee9c4
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2023-05-26 14:22:08 +0200

    Update Loading.jsx

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,7 +1,5 @@
-// import loader from "../assets/img/loader.svg";
 
 const Loading = () => {
-  //   <img src={loader} alt="loader" width={500} height={300} color={"black"} />;
   <div className="loading">Loading...</div>;
 };
 
