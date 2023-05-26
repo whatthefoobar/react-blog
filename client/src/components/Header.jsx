4e67d7f0ec932172f799128d550c98e4337d3507
Irina Munteanu Bäck <69626975+whatthefoobar@users.noreply.github.com>
2023-05-26 14:21:38 +0200

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -27,12 +27,7 @@ export default function Header() {
             </div>
           </div>
         </section>
-        {/* <div className="headerImg">
-          <img
-            src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
-            alt="nature"
-          />
-        </div> */}
+  
       </div>
       <Sidebar />
     </div>
