873ce9402999bd883197cb34d256e99270eb30ad
whatthefoobar <irina.mntn@gmail.com>
2023-09-07 12:39:51 +0200

    build is fine but when served from localhost5000 it is not

--- api/index.js
+++ api/index.js
@@ -12,8 +12,8 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
-const __dirname = path.resolve();
-app.use("/images", express.static(path.join(__dirname, "/images")));
+// const __dirname = path.resolve();
+// app.use("/images", express.static(path.join(__dirname, "/images")));
 
 connectDB();
 
@@ -37,9 +37,24 @@ app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.get("/", (req, res) => {
-  res.send("API is running....");
-});
+// app.get("/", (req, res) => {
+//   res.send("API is running....");
+// });
+
+const __dirname = path.resolve();
+app.use("/images", express.static(path.join(__dirname, "/images")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
