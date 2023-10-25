d7faf92e7b13acd3e4b7c60420011e7b29a169d7
whatthefoobar <irina.mntn@gmail.com>
2023-10-25 17:37:38 +0200

    fixed redirect bug

--- client/src/pages/Write.jsx
+++ client/src/pages/Write.jsx
@@ -30,7 +30,7 @@ export default function Write() {
     }
     try {
       const res = await axios.post("/api/posts", newPost);
-      window.location.replace("/api/post/" + res.data._id);
+      window.location.replace("/post/" + res.data._id);
     } catch (err) {
       console.log(err);
     }
