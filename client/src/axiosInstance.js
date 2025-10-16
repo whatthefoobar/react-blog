bdcc0e31cc25c0c1915fd97b8f5d7109b4397c70
whatthefoobar <irina.mntn@gmail.com>
2025-10-16 13:54:39 +0200

    ok try again, hardcoded base url

--- client/src/axiosInstance.js
+++ client/src/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+const REACT_APP_API_URL = "https://react-blog-1s9g.onrender.com";
+
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  baseURL: REACT_APP_API_URL || "http://localhost:5000",
 });
 
 export default axiosInstance;
